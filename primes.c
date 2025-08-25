6b02aa43b3a647bd6340615c4bfaff135dc857d1
MaxiFtt <maxi.fittipaldi@gmail.com>
2025-08-25 15:56:24 -0300

    progreso en primes

--- primes.c
+++ primes.c
@@ -1,7 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
 int
 main(int argc, char *argv[])
 {
-	// Your code here
+	if (argc < 2) {
+		printf("ingrese numero \n");
+		return 1;
+	}
+
+	int target = atoi(argv[1]);
+
+	int num = 2;
+
+
+	printf("primo %d \n", num);
+	int fd[2];
+
+	if (pipe(fd) == -1) return -1;
+
+	int pid = fork();
+
+	if (pid == -1) return -1;
+
+	if (pid == 0){
+		close(fd[1]);
+		read(fd[0], &num, sizeof(num));
+		printf("primo %d \n", num);
+		int value = -1;
+		while (read(fd[0], &value, sizeof(value)) != 0) {
+			if(value % num != 0){
+
+			}
+		}
+	} else {
+		close(fd[0]);
+		int i = 3;
+		while (i < target) {
+			if (i % num != 0){
+				write(fd[1], &i, sizeof(i));
+			}
+			i++;
+		}
+		close(fd[1]);
+		int status;
+		wait(&status);
+	}
+
 
 	return 0;
 }
